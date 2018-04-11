25aa24026c6b740a55c76196282a3cd924506e29
Patrick <patrickhartleyjr@gmail.com>
2018-04-10 21:44:26 -0500

    put label on top of cube

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f,20f)] float gridSize = 10f;
-	
-	// Update is called once per frame
-	void Update ()
+
+    TextMesh textMesh;
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
         Vector3 snapPos;
+
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
         transform.position = new Vector3(snapPos.x, 0, snapPos.z);
-	}
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+    }
 }
